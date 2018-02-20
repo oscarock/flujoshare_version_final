6d1048cc58fb70880121864933e77b23d2ae42c7
Oscar Diaz <o-pipe@hotmail.com>
2018-02-20 14:52:53 -0500

    Ajuste schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180212034757) do
+ActiveRecord::Schema.define(version: 20180220181539) do
 
   create_table "comentarios", force: :cascade do |t|
     t.string "idUsuario"
@@ -62,6 +62,13 @@ ActiveRecord::Schema.define(version: 20180212034757) do
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "records", force: :cascade do |t|
+    t.integer "proceso_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["proceso_id"], name: "index_records_on_proceso_id"
   end
 
   create_table "users", force: :cascade do |t|
